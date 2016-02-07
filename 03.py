8f192216d03b9b57946e65e271374324d4c8ffff
rokuden <rokuden@sfc.wide.ad.jp>
2016-02-07 17:56:08 +0900

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# coding: UTF-8
+str ="Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+str1 = str.split(" ")
+#print len(str)
+for str2 in str1:
+	print len(str2)
